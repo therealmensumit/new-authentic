d34cf5d3675d4a3686508595a3cfd7584ae14c30
therealmensumit <sk03950@gmail.com>
2022-10-22 19:00:42 +0530

    add animations

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+wow = new WOW({
+    animateClass: 'animated',
+    offset: 100,
+    callback: function(box) {
+        // console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+    }
+});
+wow.init();
