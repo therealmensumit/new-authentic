df847e72f96f3c68c89903d2a0045eef1d8a729d
therealmensumit <sk03950@gmail.com>
2022-10-24 12:14:06 +0530

    completed

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,11 @@
+$(window).scroll(function(){
+    if($(this).scrollTop() > 100){
+        $('#header').addClass('fixed');
+    } else {
+        $('#header').removeClass('fixed');
+    }
+});
+
 wow = new WOW({
     animateClass: 'animated',
     offset: 100,
